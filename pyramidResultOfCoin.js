c61aba781136a44b6b04fa812bd413a1d7d6ac21
Victor Okonkwo <victor_oko2006@yahoo.com>
2019-05-10 04:02:22 +0100

    Update pyramidResultOfCoin.js

--- pyramidResultOfCoin.js
+++ pyramidResultOfCoin.js
@@ -1,12 +1,9 @@
 <script>
-function numberDes(a,b) {
-   return b-a;
- }
 
- 
-function MinimumCoinChange(cointypes) {
+function MinimumCoinChange(cointypes, coinweight) {
     //save the coin types in coins
-    this.coins = cointypes.sort(numberDes);//cointypes;
+    this.coins = cointypes;
+    this.coinweight = coinweight;
     this.numCoinTypes = this.coins.length;
 }
 var FinalResult;
@@ -18,18 +15,37 @@ MinimumCoinChange.prototype = {
         var finalresult = [],
             totalAmount = amount;
         //loop through the coin types. for each type find how many 
-        //of the amount you can get 
+        //of the amount you can get  
         for (var i = 0; i < this.numCoinTypes; i++) {
+        
+        	//this would be used to disrupt the Greedy Recurssion
+            var currentCoinWeigth = Math.floor(this.coinweight[i]);
             //first get the value of the coin type
-            var currentTypeValue = this.coins[i],
-            //count the number of that type of coin. if there are two 25 cents
-            //then the count is two
+            var currentTypeValue = this.coins[i];
+            //count the number of that type of coin. considering the distrup
+            //before we distrup the greedy algorithm, 
+            //we need to first check if the total coin remaining can accomodate the distrup
+              
+            if(currentCoinWeigth > 0 && totalAmount > (currentTypeValue*currentCoinWeigth) )
+            {
+            	 for (var distrup = 0; distrup < currentCoinWeigth; distrup++) {
+               totalAmount -= Math.floor(currentTypeValue);
+               }
+               
+               //push the coin into result
+               finalresult.push(currentCoinWeigth);
+               
+            }
+            else
+            {
             typeCount = Math.floor(totalAmount / currentTypeValue);
             //push the coin into result
             finalresult.push(typeCount);
             //update the total amount so on the next iteration we can find the right
             //type count
             totalAmount -= (currentTypeValue * typeCount);
+            }
+           
         }
         //return the final results
         return finalresult;
@@ -41,9 +57,9 @@ MinimumCoinChange.prototype = {
     displayResults: function(amount){
         var results = this.generateCoins(amount);
         
-        FinalResult = 'There is : ';
-        //for(var i = 0; i < results.length; i++) {
-        for(var i = results.length -1; i > -1; i--) {
+        FinalResult = 'There are : ';
+        for(var i = 0; i < results.length; i++) {
+        //for(var i = results.length -1; i > -1; i--) {
           
            FinalResult +=  results[i] + ' ' + cointypes[i] + ' Kobo,  ';
            ////////////////////////////////
@@ -59,20 +75,34 @@ MinimumCoinChange.prototype = {
            console.log(str);
             
          //}
-           //document.write("\n"+"<br/>");
+          
            ///////////////////////////////
         } 
     }
 };
 
 
-var cointypes = [25, 10, 5, 1];
+var cointypes = [25, 10, 5, 1],
+	coinweight1 = [1, 4, 8, 0], //We would use this to distrup the Greedy Algorithm
+    coinweight2 = [1, 4, 9, 0], //We would use this to distrup the Greedy Algorithm
+    coinweight3 = [1, 4, 100, 0]; //We would use this to distrup the Greedy Algorithm
    // coinnames = ["25Kobo", "10Kobo", "5Kobo", "1Kobo"];
-    
-var coinChanger = new MinimumCoinChange(cointypes); //.sort()
-    
-coinChanger.displayResults(121);
+
+////    
+var coinChanger1 = new MinimumCoinChange(cointypes, coinweight1);  
+coinChanger1.displayResults(121);
 console.log(FinalResult);
+
+////
+var coinChanger2 = new MinimumCoinChange(cointypes, coinweight2);    
+coinChanger2.displayResults(121);
+console.log(FinalResult);
+
+////
+var coinChanger3 = new MinimumCoinChange(cointypes, coinweight3);    
+coinChanger3.displayResults(121);
+console.log(FinalResult);
+
 
  
   
